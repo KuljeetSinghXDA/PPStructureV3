6d6f24b3f7871a74399d14ad15cecc89d123e3e8
KuljeetSinghXDA <kspalahyia@gmail.com>
2025-10-14 14:00:37 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,6 @@ async def lifespan(app: FastAPI):
         device=DEVICE,
         enable_mkldnn=ENABLE_MKLDNN,
         enable_hpi=ENABLE_HPI,
-        cpu_threads=CPU_THREADS,
         lang=OCR_LANG,
         layout_detection_model_name=LAYOUT_DETECTION_MODEL_NAME,
         text_detection_model_name=TEXT_DETECTION_MODEL_NAME,
